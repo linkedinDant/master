cc03eadaef680218c9b5cf3fda70ca8e8ba7e9a4
o.pitton <olivier.pitton@gmail.com>
2013-05-01 12:40:56 +0200

    lecture

--- bdr/tme/tp-jointure/stat3.sql
+++
@@ -1,5 +0,0 @@
-
-
-analyze table J compute statistics;
-analyze table C compute statistics;
-analyze table F compute statistics;
--- srcs/tme/tme3/remote-registry/do-classpath-mdoc.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cd $JONAS_ROOT
-echo .$(find -iname "*.jar" -printf ":\$JONAS_ROOT/%p")
