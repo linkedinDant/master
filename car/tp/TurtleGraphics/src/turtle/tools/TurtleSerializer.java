45f4db7882168facfd4da6c68fd3263fd0469a09
pitton <olivier.pitton@gmail.com>
2013-10-09 09:51:44 +0200

    clean tp

--- car/tp/TurtleGraphics/src/turtle/tools/TurtleSerializer.java
+++ car/tp/TurtleGraphics/src/turtle/tools/TurtleSerializer.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.eclipse.emf.cdo.CDOObject;
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
@@ -26,148 +27,95 @@ import turtle.impl.TurtleFactoryImpl;
 
 public class TurtleSerializer {
 
-	public Stage loadStage(File f) {
-		ResourceSet rs = new ResourceSetImpl();
-		Resource.Factory.Registry registry = rs.getResourceFactoryRegistry();
-		Map<String,Object> m = registry.getExtensionToFactoryMap();
-		m.put("xmi", new XMIResourceFactoryImpl());
-		rs.getPackageRegistry().put(TurtlePackage.eNS_URI, TurtlePackage.eINSTANCE);
-		URI uri = URI.createFileURI(f.getAbsolutePath());
-		Resource resource = rs.getResource(uri, true);
+	@SuppressWarnings("unchecked")
+	public <T extends CDOObject> T load(File file) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource.Factory.Registry registry = resourceSet.getResourceFactoryRegistry();
+		Map<String, Object> extensionToFactoryMap = registry.getExtensionToFactoryMap();
+		extensionToFactoryMap.put("xmi", new XMIResourceFactoryImpl());
+		resourceSet.getPackageRegistry().put(TurtlePackage.eNS_URI,
+				TurtlePackage.eINSTANCE);
+		URI uri = URI.createFileURI(file.getAbsolutePath());
+		Resource resource = resourceSet.getResource(uri, true);
 		if (resource.isLoaded() && resource.getContents().size() > 0) {
-			return (Stage) resource.getContents().get(0);
+			return (T) resource.getContents().get(0);
 		}
 		return null;
 	}
-	
-	public void saveStage(Stage stage, File f) {
-		ResourceSet rs = new ResourceSetImpl();
-		Resource.Factory.Registry registry = rs.getResourceFactoryRegistry();
-		Map<String,Object> m = registry.getExtensionToFactoryMap();
-		m.put("xmi", new XMIResourceFactoryImpl());
-		m.put("ecore", new EcoreResourceFactoryImpl());
-		rs.getPackageRegistry().put(TurtlePackage.eNS_URI, TurtlePackage.eINSTANCE);
-		Resource packageResource = rs.createResource(URI.createFileURI("Turtle.ecore"));
-		packageResource.getContents().add(TurtlePackage.eINSTANCE);
-		try {
-			packageResource.load(null);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		URI uri = URI.createFileURI(f.getAbsolutePath());
-		Resource resource = rs.createResource(uri);
-		resource.getContents().add(stage);
-		try {
-			HashMap<String, Boolean> options = new HashMap<String, Boolean>();
-			options.put(XMIResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
-			resource.save(options);
-		} catch (IOException e) { e.printStackTrace(); }
-	}
-	
-	public Farm loadFarm(File f) {
-		ResourceSet rs = new ResourceSetImpl();
-		Resource.Factory.Registry registry = rs.getResourceFactoryRegistry();
-		Map<String,Object> m = registry.getExtensionToFactoryMap();
-		m.put("xmi", new XMIResourceFactoryImpl());
-		rs.getPackageRegistry().put(TurtlePackage.eNS_URI, TurtlePackage.eINSTANCE);
-		URI uri = URI.createFileURI(f.getAbsolutePath());
-		Resource resource = rs.getResource(uri, true);
-		if (resource.isLoaded() && resource.getContents().size() > 0) {
-			return (Farm) resource.getContents().get(0);
-		}
-		return null;
-	}
-	
-	public void saveFarm(Farm farm, File f) {
-		ResourceSet rs = new ResourceSetImpl();
-		Resource.Factory.Registry registry = rs.getResourceFactoryRegistry();
-		Map<String,Object> m = registry.getExtensionToFactoryMap();
-		m.put("xmi", new XMIResourceFactoryImpl());
-		m.put("ecore", new EcoreResourceFactoryImpl());
-		rs.getPackageRegistry().put(TurtlePackage.eNS_URI, TurtlePackage.eINSTANCE);
-		Resource packageResource = rs.createResource(URI.createFileURI("Turtle.ecore"));
-		packageResource.getContents().add(TurtlePackage.eINSTANCE);
-		try {
-			packageResource.load(null);
-		} catch (IOException e1) {
-			e1.printStackTrace();
-		}
-		URI uri = URI.createFileURI(f.getAbsolutePath());
-		Resource resource = rs.createResource(uri);
-		resource.getContents().add(farm);
-		try {
-			HashMap<String, Boolean> options = new HashMap<String, Boolean>();
-			options.put(XMIResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
-			resource.save(options);
-		} catch (IOException e) { e.printStackTrace(); }
-	}
-	
-	public void UpdateStage (Stage stage, Choreography choreography) {
-		stage.getChoreographies().add(choreography);
-	}
-	
-	public static void main(String[] args) {
-		
-		TurtleSerializer serializer = new TurtleSerializer();
-		
-		try {
-			// load + print du stage
-			Stage s = serializer.loadStage(new File("/workspace/TurtleGraphics/example/Stage2.xmi"));
-			System.out.println(s.display());
-		} catch(Exception e) {
-			System.err.println("Le chemin n'est pas correcte.");
-		}
-		
-		// ---------------------------------------------------------
-		
-		// load de la farm
-		Farm f = serializer.loadFarm(new File("/workspace/TurtleGraphics/example/Farm2.xmi"));
 
-		// création du stage
+	public <T extends CDOObject> void save(T object, File file) {
+		ResourceSet resourceSet = new ResourceSetImpl();
+		Resource.Factory.Registry registry = resourceSet.getResourceFactoryRegistry();
+		Map<String, Object> extensionToFactoryMap = registry.getExtensionToFactoryMap();
+		extensionToFactoryMap.put("xmi", new XMIResourceFactoryImpl());
+		extensionToFactoryMap.put("ecore", new EcoreResourceFactoryImpl());
+		resourceSet.getPackageRegistry().put(TurtlePackage.eNS_URI,
+				TurtlePackage.eINSTANCE);
+		Resource packageResource = resourceSet.createResource(URI
+				.createFileURI("Turtle.ecore"));
+		packageResource.getContents().add(TurtlePackage.eINSTANCE);
+		try {
+			packageResource.load(null);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+			return;
+		}
+		URI uri = URI.createFileURI(file.getAbsolutePath());
+		Resource resource = resourceSet.createResource(uri);
+		resource.getContents().add(object);
+		try {
+			HashMap<String, Boolean> options = new HashMap<String, Boolean>();
+			options.put(XMIResource.OPTION_SCHEMA_LOCATION, Boolean.TRUE);
+			resource.save(options);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) {
+		TurtleSerializer serializer = new TurtleSerializer();
+		// Chargement du stage
+		Stage s = serializer.load(new File("example/Stage2.xmi"));
+		System.out.println(s.display());
+		// Chargement de la ferme
+		Farm farm = serializer.load(new File("example/Farm2.xmi"));
+		// Création du stage
 		TurtleFactory factory = TurtleFactoryImpl.init();
 		Stage stage = factory.createStage();
-		
+		// Paramétrage
 		Color color = factory.createColor();
 		color.setRed(255);
 		stage.setBackgroundColor(color);
-
-		// fabrication d'une choré
-		Turtle mamaDidi = factory.createTurtle();
-		mamaDidi.setName("mamaDidi");
-		
-		// on ajoute la turtle dans la ferme
-		f.getContains().add(mamaDidi);
-		
+		Turtle turtle = factory.createTurtle();
+		turtle.setName("turtle");
+		// Ajout de la tortue dans la ferme
+		farm.getContains().add(turtle);
+		// On met le SetPen
 		SetPen setPen = factory.createSetPen();
-		setPen.setTarget(mamaDidi);
+		setPen.setTarget(turtle);
 		setPen.setUP(false);
-		
+		// On met la direction
 		Forward forward = factory.createForward();
-		forward.setTarget(mamaDidi);
+		forward.setTarget(turtle);
 		forward.setLength(100);
-
+		// On met le second SetPen
 		SetPen setPen2 = factory.createSetPen();
-		setPen2.setTarget(mamaDidi);
+		setPen2.setTarget(turtle);
 		setPen2.setUP(true);
-		
+		// Création de la chorégraphie
 		Choreography chore2 = factory.createChoreography();
 		chore2.getSequence().add(forward);
 		chore2.getSequence().add(setPen2);
 		chore2.getSequence().add(setPen);
-		
+		// On ajoute la choré / ferme dans le stage 
 		stage.getChoreographies().add(chore2);
-		stage.getFarms().add(f);
-		
-		// copie de la farm
-		File fileFarmJava = new File("/workspace/TurtleGraphics/example/Farm2-java.xmi");
-		serializer.saveFarm(f, fileFarmJava);
-		// reload de la farm pour prendre conscience qu'elle existe
-		serializer.loadFarm(fileFarmJava);
-		
-		// on peut enfin sauvegarder le stage dans un fichier
-		serializer.saveStage(stage, new File("/workspace/TurtleGraphics/example/Stage2-java.xmi"));
+		stage.getFarms().add(farm);
+		// On sauvegarde la ferme
+		File fileFarmJava = new File("example/Farm2-java.xmi");
+		serializer.save(farm, fileFarmJava);
+		// On recharge la ferme 
+		serializer.load(fileFarmJava);
+		// On sauvegarde le stage dans un fichier
+		serializer.save(stage, new File("example/Stage2-java.xmi"));
 	}
 }
-
-
-
